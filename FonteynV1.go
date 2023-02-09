ed9f394a84b2ec9a7719c3f64752689015660960
Y3llowm4n <mikegeelen2002@gmail.com>
2023-02-09 12:39:34 +0100

    V2 toegepast. Print op een bepaalde tijd

--- FonteynV1.go
+++ FonteynV1.go
@@ -1,9 +1,29 @@
+//  Het bericht krijgt het formaat: [groet]! Welkom bij Fonteyn Vakantieparken
+// Waarbij [groet] een plaataanduiding is voor
+// Goedemorgen als de huidige tijd tussen 7 uur ’s ochtends en 12 uur ’s middags ligt
+// Goedemiddag als de huidige tijd tussen 12 uur ’s middags en 18 uur ’s middags ligt
+// Goedenavond als de huidige tijd tussen 18 uur ’s middags en 23 uur ’s avonds ligt
+// Als het tussen 23 uur ’s avonds en 7 uur ’s morgens is, dient het bericht te zijn: Sorry, de parkeerplaats is ’s nachts gesloten
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 
-	fmt.Println("Welkom op fonteyn vakantieparken")
+	currentTime := time.Now()
+	groet := currentTime.Hour()
 
+	fmt.Println(groet)
+	if groet >= 7 && groet <= 11 {
+		fmt.Println("Goedenmorgen")
+	} else if groet >= 12 && groet <= 18 {
+		fmt.Println("Goedenmiddag")
+	} else if groet >= 18 && groet <= 23 {
+		fmt.Println("Goedenavond!")
+	} else {
+		fmt.Println("Sorry, de parkeerplaats is ’s nachts gesloten")
+	}
 }
