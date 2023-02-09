12aa7e68461291ce7113788b250586eec4c63baa
Y3llowm4n <mikegeelen2002@gmail.com>
2023-02-09 12:44:52 +0100

    del opdracht

--- FonteynV1.go
+++ FonteynV1.go
@@ -1,9 +1,3 @@
-//  Het bericht krijgt het formaat: [groet]! Welkom bij Fonteyn Vakantieparken
-// Waarbij [groet] een plaataanduiding is voor
-// Goedemorgen als de huidige tijd tussen 7 uur ’s ochtends en 12 uur ’s middags ligt
-// Goedemiddag als de huidige tijd tussen 12 uur ’s middags en 18 uur ’s middags ligt
-// Goedenavond als de huidige tijd tussen 18 uur ’s middags en 23 uur ’s avonds ligt
-// Als het tussen 23 uur ’s avonds en 7 uur ’s morgens is, dient het bericht te zijn: Sorry, de parkeerplaats is ’s nachts gesloten
 package main
 
 import (
